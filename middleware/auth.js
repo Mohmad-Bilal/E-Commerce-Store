0a136f5e80343dab2161e2f3bc5ef1b27711e68a
Mohammad Bilal <mhd.mhdb2.2017@gmail.com>
2026-05-03 16:30:07 +0300

    first commit

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer", "");
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findOne({ _id: decode._id, tokens: token });
+
+    if (!user) {
+      throw new Error("plz Authorize");
+    }
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (err) {
+    res.status(500).send({
+      message: err.message,
+    });
+  }
+};
+
+module.exports = auth;
---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("../db/mongoose");
+const loginRouter = require("../routes/login");
+const registerRouter = require("../routes/register");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(registerRouter);
+app.use(loginRouter);
+
+// app.use();
+
+app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+});
